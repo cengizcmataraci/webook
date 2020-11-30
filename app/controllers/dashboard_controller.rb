a7db7173de9efe950c9580f6e8d502e5d7a2cf0e
cengizcmataraci <cengizcmataraci@gmail.com>
2020-11-30 15:51:59 +0300

    Fix waiting for approve && code alignment

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -3,6 +3,7 @@ class DashboardController < ApplicationController
 
   def index
   	@books = current_user.books.includes(:comment)
+    @bookcomment = Comment.joins(:book).where(books: {user_id: current_user.id})
   	@comments = current_user.comment
   	@tradeable = current_user.tradeables
   end
--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,6 +2,5 @@ class Comment < ApplicationRecord
   belongs_to :user
   belongs_to :book
   scope :approved, -> { where(status: true) }
-  scope :unapproved, -> { where(status: false) }
   scope :waiting_for_approval, ->(user_id) { where(status: false, user_id: user_id) }
 end
